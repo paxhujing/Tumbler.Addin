0daa03ffe8bb4bc6b61ca6a606d255ee360992da
PC201608221249\Administrator <935714690@qq.com>
2016-10-09 18:38:24 +0800

    第一次提交

---
+++ Tumbler.Addin.Core/IAddinTree.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tumbler.Addin.Core
+{
+    /// <summary>
+    /// 插件树。
+    /// </summary>
+    public interface IAddinTree
+    {
+
+        void InsertAddin();
+
+        void RemoveAddin();
+    }
+}
---
+++ Tumbler.Addin.Core/ICodon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tumbler.Addin.Core
+{
+    public interface ICodon
+    {
+        IAddin Addin { get; }
+
+        String Name { get; }
+
+        String Id { get; }
+    }
+}
