134c543a8b67573f8ab247732c9e8ab45dc35c27
PC201608221249\Administrator <935714690@qq.com>
2016-10-10 18:33:32 +0800

    补提

---
+++ Tumbler.Addin.Core/AddinState.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tumbler.Addin.Core
+{
+    /// <summary>
+    /// 表示插件当前的状态。
+    /// </summary>
+    public enum AddinState
+    {
+        /// <summary>
+        /// 卸载。
+        /// </summary>
+        Uninstall,
+        /// <summary>
+        /// 依赖错误。
+        /// </summary>
+        DependencyError,
+        /// <summary>
+        /// 安装。
+        /// </summary>
+        Install,
+        /// <summary>
+        /// 启用。
+        /// </summary>
+        Enable,
+        /// <summary>
+        /// 禁用
+        /// </summary>
+        Disable,
+        /// <summary>
+        /// 更新。
+        /// </summary>
+        Update
+    }
+}
