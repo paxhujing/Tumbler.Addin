fb1bdc1706c628957d6adc89e6bdc96b254b51b0
PC201608221249\Administrator <935714690@qq.com>
2016-10-14 15:51:50 +0800

    添加IService接口

---
+++ Tumber.Addin.Common/IService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tumber.Addin.Common
+{
+    /// <summary>
+    /// 表示一个服务。
+    /// </summary>
+    public interface IService : IAddin
+    {
+    }
+}
