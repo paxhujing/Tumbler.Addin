59f859bbe2c35d3261ee688bcdb59639f27eadf7
PC201608221249\Administrator <935714690@qq.com>
2016-11-04 14:00:33 +0800

    IService接口新增Launch方法和Stop方法

--- Tumbler.Addin.Common/IService.cs
+++ Tumbler.Addin.Common/IService.cs
@@ -11,5 +11,27 @@ namespace Tumbler.Addin.Common
     /// </summary>
     public interface IService : IAddin
     {
+        #region Properties
+
+        /// <summary>
+        /// 获取一个标识。该标识表明服务是否正在运行中。
+        /// </summary>
+        Boolean IsRuning { get; }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// 启动服务。
+        /// </summary>
+        void Launch(params Object[] args);
+
+        /// <summary>
+        /// 停止服务。
+        /// </summary>
+        void Stop();
+
+        #endregion
     }
 }
